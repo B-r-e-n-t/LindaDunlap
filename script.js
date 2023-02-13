67b5a6a2bc455ce9f7f8a4bc1718c52db6d42acb
Brent <jbmsharpe@gmail.com>
2023-02-13 12:38:17 -0500

    got rid of reviews sections except on home page, added more slides to hero section

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ hamburgerButton.addEventListener('click', () => {
 
 
 
+let slidePosition = 0;
+showHeroSlides();
+
+function showHeroSlides() {
+  let i;
+  let heroSlides = document.getElementsByClassName("hero-slide-wrapper");
+  let heroDots = document.getElementsByClassName("hero-dot");
+  for (i = 0; i < heroSlides.length; i++) {
+    heroSlides[i].style.display = "none";  
+  }
+  slidePosition++;
+  if (slidePosition > heroSlides.length) {slidePosition = 1}    
+  for (i = 0; i < heroDots.length; i++) {
+    heroDots[i].className = heroDots[i].className.replace(" active", "");
+  }
+  heroSlides[slidePosition-1].style.display = "flex";  
+  heroDots[slidePosition-1].className += " active";
+  setTimeout(showHeroSlides, 2000);
+}
 
 
 
@@ -28,15 +47,15 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("reviewSlides");
-  let dots = document.getElementsByClassName("dot");
+  // let dots = document.getElementsByClassName("reviews-dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex-1].style.display = "flex";
   // dots[slideIndex-1].className += " active";
 }
